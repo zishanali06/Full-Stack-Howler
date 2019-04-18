5b151906a6ab0a32c983c54a177a2eaad2a220f2
Zishan Ali <zishanali06@gmail.com>
2019-04-17 20:08:53 -0500

    done

--- src/server/config/development.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    mysql: {
-        host: 'localhost',
-        user: 'zishan',
-        password: 'testing123',
-        database: 'chirpr',
-        connectionLimit: 10
-    }
-};
